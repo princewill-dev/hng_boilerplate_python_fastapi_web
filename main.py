77d76b3b2b6b64b443fe61de58728a8ac9cab2c7
Princewill-dev <ezekudep@gmail.com>
2024-07-13 20:50:52 +0100

    chore: Update port number in main.py

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ async def get_root(request: Request) -> dict:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=7001, reload=True)
+    uvicorn.run("main:app", port=8080, reload=True)
